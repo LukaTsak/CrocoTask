e4299ee335cfcaa3c9a60ff03940a3e785d99bec
LukaTsak <lukawaqadze01@gmail.com>
2025-10-23 16:09:20 +0400

    leaderboard modification

--- src/app/components/leaderboard/leaderboard.component.ts
+++ src/app/components/leaderboard/leaderboard.component.ts
@@ -4,10 +4,9 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
-
 @Component({
   selector: 'app-leaderboard',
-  imports: [CommonModule,RouterModule,FormsModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './leaderboard.component.html',
   styleUrl: './leaderboard.component.scss',
 })
@@ -19,11 +18,7 @@ export class LeaderboardComponent {
   constructor(private apiService: ApiServiceService) {}
 
   ngOnInit() {
-    this.apiService.getUsers().subscribe((Users) => {
-      console.log(Users);
-      this.Users = Users as any[];
-      this.FilteredUsers = [...this.Users];
-    });
+    this.generateObj(50);
   }
 
   search(searchItem: string = '') {
@@ -40,5 +35,36 @@ export class LeaderboardComponent {
         user.name.toLowerCase().includes(term) ||
         user.email.toLowerCase().includes(term)
     );
+  }
+
+  leaderboardArr: any = [];
+
+  generateObj(Quantity: number) {
+    for (let i = 0; i < Quantity; i++) {
+      const weekTypes = ['I', 'II', 'III', 'IV'];
+      const customerId = i + 1;
+      const loginName = this.generateRandString(Math.floor(Math.random() * (15 - 5 + 1) + 5));
+      const place = Math.floor(Math.random() * Quantity);
+      const week = weekTypes[Math.floor(Math.random() * weekTypes.length)];
+
+      this.leaderboardArr.push({
+        customerId: customerId,
+        loginName: loginName,
+        place: place,
+        week: week,
+      });
+      // console.log(this.leaderboardArr);
+    }
+  }
+
+  generateRandString(length: number) {
+    let string = '';
+    const characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i <= length; i++) {
+      string += characters[Math.floor(Math.random() * characters.length)];
+    }
+    console.log(string);
+    return string;
   }
 }
